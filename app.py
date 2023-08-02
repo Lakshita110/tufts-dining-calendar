9344697fcd50d784235082b5493eb06cb04b9d8c
Lakshita Jain <Lak.Jai110@gmail.com>
2023-08-02 13:51:08 +0200

    Gets all data for a week

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 from google_calendar import GoogleCalendar
 from tufts_dining import TuftsDining
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import re 
 
 def main():
-    date = datetime.today().strftime("%Y-%m-%d")
+    date = datetime.today()
+    date_text = date.strftime("%Y-%m-%d")
     dining = TuftsDining()
     calendar = GoogleCalendar()
     locations = [0,1]
 
-    for index in locations:
-        text = dining.get_data(index, date) 
-        if text: 
-            location = dining.LOCATIONS[index]
-            summary = f"{location[0].split()[0]} Menu"
-            event = calendar.create_event(summary=summary, description=text, date=date)
-            calendar.insert_event(event)
-        else:
-            print(f"Not creating event because is not serving food :(")
+    for i in range(0,7):
+        for index in locations:
+            text = dining.get_data(index, date_text) 
+            if text: 
+                location = dining.LOCATIONS[index]
+                summary = f"{location[0].split()[0]} Menu"
+                event = calendar.create_event(summary=summary, description=text, date=date_text)
+                calendar.insert_event(event)
+            else:
+                print(f"Not creating event because is not serving food :(")
+
+        #Update for next day        
+        date = date + timedelta(days=1)
+        date_text = date.strftime("%Y-%m-%d")
 
 if __name__ == '__main__':
     main()
